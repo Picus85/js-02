1db6ca00fd58178dd747fac67c00e559d285e0cc
Picus85 <sergiypiknevych@gmail.com>
2018-12-19 08:35:43 +0000

    3 methods

--- js/index.js
+++ js/index.js
@@ -3,13 +3,13 @@ let n = prompt("Input n")
 function fibonNum(n) {
     if (n == 0) { return (0); }
     if (n <= 2) {
-       return (1);
-   }
-  if (n > 2) {
-     return fibonNum(n - 1) + fibonNum(n - 2);
-   }
+        return (1);
+    }
+    if (n > 2) {
+        return fibonNum(n - 1) + fibonNum(n - 2);
+    }
 }
-document.write(fibonNum(n)+"<br>")
+document.write("N-не число фібоначі методом рекурсії-  "+fibonNum(n) + "<br>")
 
 
 let arayFibon = [1, 1];
@@ -19,8 +19,34 @@ function fibonMatrix(n) {
         if (n > 2) {
             arayFibon.push(arayFibon[i - 1] + arayFibon[i]);
         }
-        document.write(arayFibon[i - 1]+" ");
+        document.write(arayFibon[i - 1] + " ");
     }
     return arayFibon[n - 1];
 }
-document.write(fibonMatrix(n));
+document.write("Ряд фібоначчі методом матриці -  "+fibonMatrix(n)+"<br>");
+
+
+
+function variable(n) {
+    let variableNumFirst;
+    let variableNumSecond;
+    let variableNum;
+    for (let i = 1; i <= n; i++) {
+        if (i == 1) {
+             variableNumFirst = 1;
+             variableNum = variableNumFirst;
+        }
+        if (i == 2) {
+            variableNumSecond = 1;
+            variableNum = variableNumSecond;
+        }
+        if (i > 2) {
+            variableNum = variableNumFirst + variableNumSecond;
+            variableNumSecond=variableNumFirst;
+            variableNumFirst = variableNum;
+        }
+        
+    }
+    return variableNum;
+}
+document.write("N-не число фібоначчі методом змінних -  "+variable(n));
